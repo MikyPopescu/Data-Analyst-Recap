02630d39b0a2914f6dae8131dfc2b99d394c4702
mikypopes <miky.popescu@oracle.com>
2026-09-11 10:42:59 +0300

    case, subqueries

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -125,4 +125,78 @@ SELECT
 FROM employee_demographics;
 
 # CASE
+SELECT  
+    first_name,
+    last_name,
+    CASE
+        WHEN age<=30 THEN 'Young'
+        WHEN age BETWEEN 31 AND 50 THEN 'Old'
+        WHEN age >= 50 THEN 'Very old'
+    END AS Age_Braket
+FROM 
+    employee_demographics
+;
 
+
+-- Pay increase and bonus
+-- < 50000 = 5%
+-- > 50000 = 7%
+-- Finance = 10% bonus
+SELECT 
+    first_name,
+    last_name,
+    salary,
+    CASE
+        WHEN salary < 50000 THEN salary + (salary*0.05)
+        WHEN salary > 50000 THEN salary + (salary*0.07)
+    END AS New_Salary,
+    CASE
+        WHEN dept_id = 6 THEN salary * .10
+    END AS Bonus
+FROM 
+    employee_salary
+;
+
+-- Subqueries
+-- in WHERE / SELECT /FROM
+SELECT 
+    *
+FROM 
+    employee_demographics
+WHERE
+    employee_id IN 
+                    ( SELECT 
+                           employee_id 
+                      FROM 
+                           employee_salary
+                      WHERE
+                            dept_id =1
+
+);
+
+
+SELECT 
+    first_name,
+    salary,
+    (SELECT AVG(salary) FROM employee_salary)
+FROM 
+    employee_salary
+;
+
+
+SELECT AVG(max_age) -- , AVG(`MAX(age)`)
+FROM
+    (SELECT 
+        gender,
+        AVG(age) AS avg_age, 
+        MAX(age) AS max_Age, 
+        MIN(age) AS min_age, 
+        COUNT(age) AS count_age
+    FROM 
+        employee_demographics
+    GROUP BY 
+        gender
+    ) as agg_tbl
+GROUP BY gender;
+
+# Window functions
