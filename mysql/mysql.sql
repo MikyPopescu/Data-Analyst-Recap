ffa325d050fb276505edf9108224ad9ecfb2d92d
mikypopes <miky.popescu@oracle.com>
2026-09-11 15:01:47 +0300

    CTEs and TEMP tables

--- mysql/mysql.sql
+++ mysql/mysql.sql
@@ -260,3 +260,91 @@ JOIN
 ON 
     dem.employee_id=sal.employee_id
 ;
+
+#CTEs
+WITH CTE_Example (Gender, AVG_Sal, MAX_Sal, MIN_Sal, COUNT_Sal) AS 
+(
+SELECT
+    gender,
+    AVG(salary) avg_sal,
+    MAX(salary) max_sal,
+    MIN(salary) min_sal,
+    COUNT(salary) count_sal
+FROM
+    employee_demographics dem
+JOIN
+    employee_salary sal
+ON
+    dem.employee_id = sal.employee_id
+GROUP BY
+    gender
+)
+SELECT avg_sal FROM CTE_Example
+;
+
+
+-- echivalent
+SELECT AVG(avg_sal)
+FROM
+    (SELECT
+        gender,
+        AVG(salary) avg_sal,
+        MAX(salary) max_sal,
+        MIN(salary) min_sal,
+        COUNT(salary) count_sal
+    FROM
+        employee_demographics dem
+    JOIN
+        employee_salary sal
+    ON
+        dem.employee_id = sal.employee_id
+    GROUP BY
+        gender
+    ) AS example_subquery
+;
+
+
+WITH CTE_Example AS 
+(
+SELECT
+    employee_id,
+    gender,
+    birth_date
+FROM
+    employee_demographics dem
+WHERE
+    birth_date > '1985-01-01'
+),
+CTE_Example2 AS
+(
+    SELECT employee_id, salary
+    FROM employee_salary
+    WHERE salary> 50000
+)
+SELECT * 
+FROM 
+    CTE_Example
+JOIN 
+    CTE_Example2
+ON 
+    CTE_Example.employee_id = CTE_Example2.employee_id
+;
+
+# Temporary tables
+CREATE TEMPORARY TABLE temp_table
+(
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    favortie_movie VARCHAR(100)
+);
+
+INSERT INTO temp_table VALUES ('A', 'B', 'Lord of the Rings');
+
+SELECT * FROM temp_table;
+
+CREATE TEMPORARY TABLE salary_over_50k 
+SELECT * 
+FROM employee_salary
+WHERE salary>=50000;
+
+SELECT * FROM salary_over_50k;
