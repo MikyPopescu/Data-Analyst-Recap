5a814e199b3863134f658396d277733036e8bb7a
mikypopes <miky.popescu@oracle.com>
2026-09-22 13:28:04 +0300

    checked null values removed columns and rows

--- mysql/Project1.sql
+++ mysql/Project1.sql
@@ -120,12 +120,9 @@ UPDATE layoffs_staging2
 SET industry = 'Crypto'
 WHERE industry LIKE 'Crypto%';
 
-
-
 SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
 FROM layoffs_staging2
 ORDER BY 1;
-
 
 UPDATE layoffs_staging2
 SET country=TRIM(TRAILING '.' FROM country)
@@ -141,4 +138,51 @@ SET `date`= STR_TO_DATE(`date`,'%Y-%m-%d');
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;
 
-SELECT * FROM layoffs_staging2;
+
+
+# Null values
+SELECT * 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry is NULL OR industry='';
+
+SELECT *
+FROM layoffs_staging2
+WHERE company LIKE 'Airbnb'; -- Industry is Travel
+
+SELECT t1.industry, t2.industry -- blank and not blank i should populate with t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+  ON t1.company=t2.company
+  AND t1.location=t2.location
+WHERE (t1.industry IS NULL OR t1.industry='') AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging2 
+SET industry= NULL 
+WHERE industry='';
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+  ON t1.company=t2.company
+SET t1.industry=t2.industry
+WHERE (t1.industry IS NULL OR t1.industry='') AND t2.industry IS NOT NULL; -- this did not work without the update above due to blank VALUES
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
+
+SELECT *
+FROM layoffs_staging2;
