ca9122e3c72fb917c6fb2a76e599c0ecc7aec59a
mikypopes <miky.popescu@oracle.com>
2026-09-22 13:07:54 +0300

    data standardization

--- mysql/Project1.sql
+++ mysql/Project1.sql
@@ -1,4 +1,7 @@
+
 CREATE DATABASE world_layoffs;
+
+USE world_layoffs;
 
 CREATE TABLE `layoffs` (
   `company` text,
@@ -63,3 +66,79 @@ WITH duplicate_cte AS
     FROM layoffs_staging
 )
 DELETE FROM duplicate_cte WHERE row_num>1; -- MySQL Error (1288): The target table duplicate_cte of the DELETE is not updatable
+
+
+CREATE TABLE `layoffs_staging2` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+;
+
+select * from layoffs_staging2;
+
+INSERT INTO layoffs_staging2
+SELECT *,
+ROW_NUMBER() OVER(
+  PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions
+) AS row_num
+FROM layoffs_staging;
+
+select * from layoffs_staging2
+where row_num>1; -- duplicates
+
+
+DELETE
+FROM layoffs_staging2
+where row_num>1; -- removed duplicates
+
+
+-- Standardizing data
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+
+UPDATE layoffs_staging2
+SET company = trim(company);
+
+SELECT DISTINCT industry
+FROM layoffs_staging2
+ORDER BY 1;
+
+SELECT industry
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+
+UPDATE layoffs_staging2
+SET country=TRIM(TRAILING '.' FROM country)
+WHERE country LIKE 'United States%';
+
+SELECT `date`,
+STR_TO_DATE(`date`,'%Y-%m-%d')
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date`= STR_TO_DATE(`date`,'%Y-%m-%d');
+
+ALTER TABLE layoffs_staging2
+MODIFY COLUMN `date` DATE;
+
+SELECT * FROM layoffs_staging2;
